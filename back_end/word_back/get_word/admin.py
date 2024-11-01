281f09e9902ddf66ce7fe428e69a4b19ae6689b2
Azzi <azim1qq.com>
2024-11-02 05:15:48 +0800

    v1

--- back_end/word_back/get_word/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from get_word import models as md
-
-admin.site.register(md.Words)
--- back_end/word_back/get_word/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.get_my_word, name="get_my_word"),
-    path("add/", views.add_word, name="add_my_word"),
-]
